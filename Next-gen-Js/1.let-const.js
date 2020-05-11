d957dff70706041d0b30b4ffe202e1b43505d9e3
Benjamin <benjaminbotros@outlook.com>
2020-05-11 19:12:29 +0200

    Understanding "let" and "const"

---
+++ Next-gen-Js/1.let-const.js
@@ -0,0 +1,6 @@
+var oldVariable = "old"; //Old way to create variables
+
+let newVariable = "new"; //New way to create a variable
+const newConstantVariable = "new unchanging"; //New way to create a variable that doesn't change
+
+console.log(oldVariable + " | " + newVariable +" | "+ newConstantVariable);
